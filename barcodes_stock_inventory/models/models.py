7ab79cd4e28ff3de1ea9b0551ec37b47168d227b
KolushovAlexandr <kolushov@it-projects.info>
2018-04-06 13:51:25 +0500

    [LINT]

--- barcodes_stock_inventory/models/models.py
+++ barcodes_stock_inventory/models/models.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Kolushov Alexandr <https://it-projects.info/team/KolushovAlexandr>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models, api
+from odoo import models
 
 
 class BarcodeStockInventory(models.Model):
